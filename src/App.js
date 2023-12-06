f8accc3c9e3642866bd351f29f7b2bd37b9bef40
sanjeewna <sanjeewnadevi93@gmail.com>
2023-12-06 06:21:35 +0200

    week8-lab6 completed

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>useState Hook Lab</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
